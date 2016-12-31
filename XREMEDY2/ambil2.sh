7948fe989d1bbe1ae92669bc36b4fdf3160c3952
Andhita Nurul Ainun - 1406543782 <andhita.nurul@badak.cs.ui.ac.id>
2016-12-31 20:57:47 +0700

    rem-uas babak4

--- XREMEDY2/ambil2.sh
+++ XREMEDY2/ambil2.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 # (c) 2016 Andhita Nurul Ainun -- This is free software
 # START Fri Dec 30 22:03:00 WIB 2016
-# program ini terlebih dahulu mengambil list situs tanpa keterangan nomor, kemudian mengambil situs perbaris yang kemudian di lakukan dump lalu dimasukkan kedalam file yang berkesesuaian menggunakan loop. jika dump gagal, dimasukkan kedalam RK-XX-GAGAL.txt
-
-
-#simpan link saja
-a="$(awk '{print $3}' ranking.txt)"
-
-#simpan RK-XX
-b="$(awk '{print $2}' ranking.txt)"
+# program ini terlebih dahulu mengambil list situs tanpa keterangan nomor dan dimasukkan kedalam array, kemudian mengambil nomor masing-masing situs perbaris yang dimasukkan juga kedalam array. Setelah itu di lakukan for loop untuk dump lalu dimasukkan kedalam file yang berkesesuaian dengan nomor situsnya. dilakukan juga pengecekan jika dump gagal,yang kemduian dimasukkan kedalam RK-XX-GAGAL.txt
 
 
 arrSitus=()
 arrNomor=()
 
-for val in $a; do
+for val in $(awk '{print $3}' ranking.txt); do
 arrSitus+=("$val")
 done
 
-for val2 in $b; do
+for val2 in $(awk '{print $2}' ranking.txt); do
 arrNomor+=("$val2")
 done
 
 for (( i=0; i<${#arrNomor[@]}; i++ )); do 
-      link="$(w3m -dump ${arrSitus[i]})"
+      link="$(w3m -dump -cols 2000 ${arrSitus[i]})"
       if [ -z "$link" ]; then
-	 touch "${arrNomor[i]}-GAGAL.txt"
+	touch "${arrNomor[i]}-GAGAL.txt"
       else
-       w3m -dump -cols 120 ${arrSitus[1]} > "${arrNomor[i]}.txt" 
+       w3m -dump -cols 2000 ${arrSitus[i]} > "${arrNomor[i]}.txt" 
        fi 	  
 done  
