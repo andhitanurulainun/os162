9a135a8b4250c9b8f51352faa797726bf40d797c
Andhita Nurul Ainun - 1406543782 <andhita.nurul@badak.cs.ui.ac.id>
2016-12-31 20:16:56 +0700

    rem-uas babak 4

--- XREMEDY2/ambil2.sh
+++ XREMEDY2/ambil2.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 # (c) 2016 Andhita Nurul Ainun -- This is free software
 # START Fri Dec 30 22:03:00 WIB 2016
-#
+# program ini terlebih dahulu mengambil list situs tanpa keterangan nomor, kemudian mengambil situs perbaris yang kemudian di lakukan dump lalu dimasukkan kedalam file yang berkesesuaian menggunakan loop. jika dump gagal, dimasukkan kedalam RK-XX-GAGAL.txt
 
-declare -i n
-n=1
-sed -n 's/^.*http/http/p' ranking.txt |
-while read LINE
-do
-situs="$(w3m -dump -cols 120 $LINE)"
-if [ n < 10 ]; then
- echo "$situs" > "RK-0$n.txt"
-else
- echo "$situs" > "RK-$n.txt"
-fi
-if [ "$n == 7" ]; then
-  n=$((n+2))
-else
-  n=$((n+1))
-if [ "$n == 31" ]; then
-  n=$((n+2))
-else
-  n=$((n+1))
-if [ "$n == 46" ]; then
-  n=$((n+2))
-else
-  n=$((n+1))
-fi
+
+#simpan link saja
+a="$(awk '{print $3}' ranking.txt)"
+
+#simpan RK-XX
+b="$(awk '{print $2}' ranking.txt)"
+
+
+arrSitus=()
+arrNomor=()
+
+for val in $a; do
+arrSitus+=("$val")
 done
 
+for val2 in $b; do
+arrNomor+=("$val2")
+done
+
+for (( i=0; i<${#arrNomor[@]}; i++ )); do 
+      link="$(w3m -dump ${arrSitus[i]})"
+      if [ -z "$link" ]; then
+	 touch "${arrNomor[i]}-GAGAL.txt"
+      else
+       w3m -dump -cols 120 ${arrSitus[1]} > "${arrNomor[i]}.txt" 
+       fi 	  
+done  
